f23e6edb9cfdd7895809e1de6e48f5bec070fa54
bawik ardiyan <ardiyanramadhan4@gmail.com>
2019-02-27 10:29:11 +0700

    Update Home.php

---
+++ Framework/Home.php
@@ -0,0 +1,433 @@
+<?php
+include '../function.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/pdjember.jpg" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: BizPage
+    Theme URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo">
+          <h1 class="text-white" style="font-family: Vendana;"><img src="img/pdjember.jpg" width="70" height="70"> Perisai Diri Jember</h1>
+          
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#intro"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+<nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li><a href="#intro">Home</a></li>
+          <li><a href="agenda.php">Agenda</a></li>
+          <li><a href="#services">Informasi</a></li>
+          <li><a href="#portfolio">Unit Latihan</a></li>
+          <li><a href="#team">Atlet</a></li>
+          <li class="menu-has-children"><a href="">UKT</a>
+            <ul>
+              <li><a href="#">Pendaftaran UKT</a></li>
+              <li><a href="#">Materi UKT</a></li>
+              <li><a href="#">Hasil UKT</a></li>
+              <li><a href="#">Pelantikan</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Kontak</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+<section id="intro">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <div class="carousel-item active">
+            <div class="carousel-background"><img src="img/1.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>Salam Bunga Sepasang</h2>
+                <p>Pandai Silat Tanpa Cidera</p>
+                <a href="#KelolahAkun" class="btn-get-started scrollto">Daftar Akun</a>
+            </div>
+          </div>
+        </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/2.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>Salam Bunga Sepasang</h2>
+                <p>Jika anda tidak bisa melakukan hal hebat maka lakukanlah hal-hal kecil dengan cara yang hebat.</p>
+                <a href="#KelolahAkun" class="btn-get-started scrollto">Daftar Akun</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/3.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>Salam Bunga Sepasang</h2>
+                <p>Tujuan berlatih ilmu silat ialah untuk memelihara kesehatan, ketenangan, dan kepercayaan kepada diri sendiri. Dilarang untuk berkelahi, sombong, mencari musuh dan berbuat apapun yang tidak baik untuk pribadi maupun pihak lain. Pokoknya semua itu untuk keselamatan dan kebaikan budi. Itulah Perisai Diri yang ampuh</p>
+                <a href="#KelolahAkun" class="btn-get-started scrollto">Daftar Akun</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/4.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>Salam Bunga Sepasang</h2>
+                <p>Pesilat juga bisa lelah namun pesilat tidak mudah menyerah</p> 
+                <a href="#KelolahAkun" class="btn-get-started scrollto">Daftar Akun</a>             
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#introCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon ion-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      </div>
+    </div>
+  </section><!-- #intro -->
+
+<section id="about">
+      <div class="container">
+
+        <header class="section-header">
+          <img src="img/PD.jpg" style="margin-left: 45%;" width="100" height="100">
+          <h3 class="mt-1">Perisai Diri</h3>
+          <p>Keluarga Silat Nasional Indonesia Perisai Diri atau disingkat Kelatnas Indonesia Perisai Diri adalah organisasi olahraga beladiri yang didirikan oleh R.M. Soebandiman Dirdjoatmodjo pada tanggal 2 Juli 1955 di Surabaya, Jawa Timur. Kelatnas Indonesia Perisai Diri merupakan salah satu anggota IPSI (Ikatan Pencak Silat Indonesia). induk organisasi resmi pencak silat di Indonesia di bawah naungan KONI (Komite Olahraga Nasional Indonesia). Kelatnas Indonesia Perisai Diri merupakan salah satu dari 10 perguruan pencak silat yang mendapatkan predikat Perguruan Historis Pencak Silat. R.M. Soebandiman Dirdjoatmodjo lahir di Yogyakarta pada tanggal 8 Januari 1913 di lingkungan pura Pakualaman.</p>
+        </header>
+
+        <div class="row about-cols">
+          <?php
+          $agenda = mysqli_query($koneksi,"SELECT * FROM agenda order by Kode_Agenda desc limit 1");
+            while ($res=mysqli_fetch_array($agenda)) { ?>
+          <div class="col-md-4 wow fadeInUp">
+            <div class="about-col">
+              <div class="img">
+                <img src="../Admin/uploads/<?php echo $res ['Gambar'] ?>" alt="" class="img-fluid">
+                <div class="icon mb-4"><i class="ion-ios-list-outline"></i></div>
+              </div>
+              <h1 class="kategori text-dark text-center">Agenda</h1>
+              <h2 class="title"><a href="#"><?php echo $res['Judul_Agenda']; ?></a></h2>
+              <p><?php echo substr($res['Isi_Agenda'],0,300); ?>...</p>
+            </div>
+          </div>
+        <?php } ?>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-plan.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-list-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Our Plan</a></h2>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-vision.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-eye-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Our Vision</a></h2>
+              <p>
+                Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #about -->
+<section id="KelolahAkun">
+<div class="container">
+<div class="row mb-4">
+<div class="col">
+  <h5>Masuk, Daftar Akun PD anda</h5>
+<button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Login</button>
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="login-form col-md-12">
+                <div class="modal-header">
+                <h5 class="text-dark"><img src="img/icon.png" width="75" height="75" style="" align="center"> Login User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                <form action="login/login.php" method="post">
+
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="Username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="Password" class="form-control">
+                </div>
+                <button class="btn btn-primary btn-block" name="daftar">Login</button>
+                <br/>
+                </div>
+                </form>
+                </div>
+                </div>
+                </div>
+                </div>
+</div>
+</div>
+<div class="row mb-4">
+<div class="col">
+              <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModalregis">Daftar Akun</button>
+                <div class="modal fade" id="exampleModalregis" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="register-form col-md-12">
+                <form action="login/daftar.php" method="post">
+                <div class="modal-header mb-2">
+                    <h5 class="text-dark"><img src="img/registrasi.png" class="ikon" width="75" height="75">Daftar Akun</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="Username" id="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="Password" id="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Konfirmasi Password</label>
+                    <input type="password" name="Password2" id="password2" class="form-control">
+                </div>
+             
+                <div class="form-group">
+                    <label>Alamat</label>
+                    <input type="text" name="Alamat" id="alamat" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>E-Mail</label>
+                    <input type="email" name="Email" id="email" class="form-control">
+                </div>
+                <br/>
+                    <input type="submit" class="btn btn-primary btn-block" name="register"><a href="login.html"></a></input>
+                <br/>
+            </form>
+            </div>
+            </div>
+            </div>
+            </div>
+</div>
+</div>
+<div class="row mb-4">
+<div class="col">
+               <button class="btn btn-danger" data-toggle="modal" data-target="#exampleModalforgot">Lupa Password</button>
+                <div class="modal fade" id="exampleModalforgot" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="forgot-form col-md-12">
+        <form action="login/forgot.php" method="post">
+            <div class="modal-header">
+                <h5 class="text-dark"><img src="img/FP.png" class="ikon" width="75" height="75">Lupa Password</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="Username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>E-Mail</label>
+                <input type="email" name="Email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Password Baru</label>
+                <input type="password" name="Password" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Konfirmasi Password Baru</label>
+                <input type="password" name="Password2" id="password2" class="form-control">
+            </div>
+            <br/>
+            <input type="submit" class="btn btn-primary btn-block" name="forgot"><a href="login.html"></a></input>
+            <br/>
+        </form>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</section>
+
+<section id="Kritik" class="mt-5">
+  <div class="container">
+    <div class="row">
+      <header class="section-header">
+        <h1>Kritik, Saran dan Masukan</h1>
+      </header>
+    </div>
+    <div class="col-md-9 wow fadeInUp" data-wow-delay="0.1s">
+      <div class="about-col">
+        <form action="" method="post" enctype="multipart/form-data">
+  <div class="form-group">
+    <label for="exampleInputName">Nama</label>
+    <input name="Nama" class="form-control" aria-describedby="name" placeholder="Masukkan Nama">
+    <br/>
+  <div class="form-group">
+    <label for="exampleInputName">Email</label>
+    <input type="email" name="Email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan Email">
+  <div class="form-group">
+                      <label style="margin-right: 45px; margin-top: 10px; font-family: Times New Roman; font-size: 13pt;">Kritik, Saran, dan Masukan</label>
+      <textarea name="Komentar" class="form-control" placeholder="Kritik, Saran, dan Masukan"> </textarea>
+</div>
+<br/>
+  <button type="submit" class="btn btn-primary" name="submit">Kirim</button>
+</form>
+  </div>
+</section>
+                
+
+
+<footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-8 footer-info">
+            <h3>Perisai Diri Jember</h3>
+            <p></p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Menu</h4>
+            <ul>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#"></a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#"></a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#"></a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#"></a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-8 footer-contact">
+            <h4>Alamat</h4>
+            <p>
+              Jl. Letjen Panjahitan<br>
+              Jember, 68122<br>
+              Indonesia <br>
+              <strong>Phone:</strong> 081235655001<br>
+              <strong>Email:</strong> pdjember@perisaidiri.ac.id<br>
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+          </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>pdjember.2018.</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
+        -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+<script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
